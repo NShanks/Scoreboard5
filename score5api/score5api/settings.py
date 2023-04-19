776ed4a62327d6eb0d40e551b6cacd8020f53728
Nicolas Shanks <nicolasshanks10@gmail.com>
2023-04-19 12:25:35 -0400

    more django backend stuff added

--- score5api/score5api/settings.py
+++ score5api/score5api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gameInfo.apps.GameinfoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
